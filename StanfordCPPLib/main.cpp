8b08d0041880b3d34fd9f642142f4958750b68ed
Marty Stepp <stepp@cs.washington.edu>
2015-04-09 16:49:35 -0700

    fixed small bug about warning on unused field 'argv' in main.cpp

--- StanfordCPPLib/main.cpp
+++ StanfordCPPLib/main.cpp
@@ -6,6 +6,8 @@
  * own module to ensure that it is loaded only if the client doesn't
  * supply one.
  * 
+ * @version 2014/12/02
+ * - fixed compiler warning about unused 'argv' parameter
  * @version 2014/10/22
  * - made it work when console.h is not included (uses plain text console)
  * @version 2014/10/08
@@ -16,7 +18,7 @@
 #include <iostream>
 
 #ifndef SPL_AUTOGRADER_MODE
-int Main(int, char* argv[]) {
+int Main(int, char* /*argv*/[]) {
     extern int Main();
     return Main();
 }
