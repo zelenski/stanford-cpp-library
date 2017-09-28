fef629108a4a2d39c4df2ddbd0e1737bc7599d46
Marty Stepp <stepp@cs.stanford.edu>
2017-09-28 16:03:07 -0700

    bug fix for pointer/int comparison in call_stack_gcc

--- StanfordCPPLib/system/call_stack_gcc.cpp
+++ StanfordCPPLib/system/call_stack_gcc.cpp
@@ -4,6 +4,8 @@
  * Linux/gcc implementation of the call_stack class.
  *
  * @author Marty Stepp, based on code from Fredrik Orderud
+ * @version 2017/09/02
+ * - small bug fix for new clang warning about ordered comparison between pointer and zero
  * @version 2016/12/01
  * - bug fixes for call stack line number retrieval
  * @version 2016/10/14
@@ -286,7 +288,7 @@ call_stack::call_stack(const size_t /*num_discard = 0*/) {
             // by subtracting them we get the offset of the function within the file
             // which addr2line can use to look up function line numbers.
 
-            if (dlinfo.dli_fbase > 0 && trace[i] >= dlinfo.dli_fbase) {
+            if (dlinfo.dli_fbase > (void*) 0 && trace[i] >= dlinfo.dli_fbase) {
                 e.address2 = (void*) ((long) trace[i] - (long) dlinfo.dli_fbase);
             } else {
                 e.address2 = dlinfo.dli_saddr;
