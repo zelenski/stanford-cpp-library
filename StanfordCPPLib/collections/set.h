298c3f60a1d7ffaa0ca549dd28d2080362ac7677
Keith Schwarz <htiek@cs.stanford.edu>
2019-02-07 16:20:51 -0800

    Bug fixes for static asserts; better static assertions for sets.

--- StanfordCPPLib/collections/set.h
+++ StanfordCPPLib/collections/set.h
@@ -44,6 +44,7 @@
 #include <iostream>
 #include <set>
 #include <functional>
+#include <type_traits>
 
 #define INTERNAL_INCLUDE 1
 #include "collections.h"
@@ -59,6 +60,87 @@ namespace stanfordcpplib {
             using MapType   = Map<T, bool>;
             static std::string name() {
                 return "Set";
+            }
+
+            /* The Set type does allow you to construct the underlying Map by handing
+             * along a std::function.
+             */
+            template <typename Function>
+            static MapType construct(Function comparator) {
+                static_assert(std::is_assignable<std::function<bool(const ValueType&, const ValueType&)>, Function>::value,
+                              "Oops! Seems like you tried to initialize a Set incorrectly. Click here for details.");
+                /*
+                 * Hello student! If you are reading this message, it means that you tried to
+                 * initialize a Set improperly. For example, you might have tried to write
+                 * something like this:
+                 *
+                 *     Set<int> mySet = 137; // Oops!
+                 *
+                 * Here, for example, you're trying to assign an int to a Set<int>.
+                 *
+                 * or perhaps you had a function like this one:
+                 *
+                 *     void myFunction(Set<int>& mySet);
+                 *
+                 * and you called it by writing
+                 *
+                 *     myFunction(someSet + someOtherSet); // Oops!
+                 *     myFunction({ });                    // Oops!
+                 *
+                 * In these cases, you're trying to pass a value into a function that takes
+                 * its argument by (non-const) reference. C++ doesn't allow you to do this.
+                 *
+                 * To see where the actual error comes from, look in the list of error messages
+                 * in Qt Creator. You should see a line that says "required from here" that
+                 * points somewhere in your code. That's the actual line you wrote that caused
+                 * the problem, so double-click on that error message and see where it takes
+                 * you. Now you know where to look!
+                 *
+                 * Hope this helps!
+                 */
+                return MapType(comparator);
+            }
+
+            /* You can also default-construct it. */
+            static MapType construct() {
+                return {};
+            }
+
+            /* However, you can't pass in any other arguments. */
+            template <typename... Args>
+            static void construct(MapType &, Args&&...) {
+                static_assert(Fail<Args...>::value, "Oops! Seems like you tried to initialize a Set incorrectly. Click here for details.");
+
+                /*
+                 * Hello student! If you are reading this message, it means that you tried to
+                 * initialize a Set improperly. For example, you might have tried to write
+                 * something like this:
+                 *
+                 *     Set<int> mySet = 137; // Oops!
+                 *
+                 * Here, for example, you're trying to assign an int to a Set<int>.
+                 *
+                 * or perhaps you had a function like this one:
+                 *
+                 *     void myFunction(Set<int>& mySet);
+                 *
+                 * and you called it by writing
+                 *
+                 *     myFunction(someSet + someOtherSet); // Oops!
+                 *     myFunction({ });                    // Oops!
+                 *
+                 * In these cases, you're trying to pass a value into a function that takes
+                 * its argument by (non-const) reference. C++ doesn't allow you to do this.
+                 *
+                 * To see where the actual error comes from, look in the list of error messages
+                 * in Qt Creator. You should see a line that says "required from here" that
+                 * points somewhere in your code. That's the actual line you wrote that caused
+                 * the problem, so double-click on that error message and see where it takes
+                 * you. Now you know where to look!
+                 *
+                 * Hope this helps!
+                 */
+                error("static_assert failed?");
             }
         };
     }
