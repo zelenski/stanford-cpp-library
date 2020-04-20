c0362a5cf2471b6ae6f37054e32a26df2cc2e681
Cathy Zhang <cathy2zhang@gmail.com>
2020-04-19 21:30:40 -0700

    Add unit tests for trim functions

--- SPL-unit-tests/src/test-strlib.cpp
+++ SPL-unit-tests/src/test-strlib.cpp
@@ -18,11 +18,49 @@ PROVIDED_TEST("Conversions between string and integer") {
     Vector<int> outB = {0, 77, 333, -4, -59, 123, -45};
 
     for (int i = 0; i < inB.size(); i++) {
-         EXPECT_EQUAL(stringToInteger(inB[i]), outB[i]);
+        EXPECT_EQUAL(stringToInteger(inB[i]), outB[i]);
     }
 
     Vector<string> inError = {"apple", "", "-", "- 59", "123a", "--5", "2.5", "#1", "5 9"};
     for (auto& e: inError) {
-          EXPECT_ERROR(stringToInteger(e));
-     }
+        EXPECT_ERROR(stringToInteger(e));
+    }
+}
+
+PROVIDED_TEST("Trimming whitespace characters") {
+    /* Trim from beginning and end */
+    EXPECT_EQUAL(trim(""), "");
+    EXPECT_EQUAL(trim(" "), "");
+    EXPECT_EQUAL(trim("\ttab\t"), "tab");
+    EXPECT_EQUAL(trim("newline\n"), "newline");
+    EXPECT_EQUAL(trim("\t\n"), "");
+
+    EXPECT_EQUAL(trim(" leading"), "leading");
+    EXPECT_EQUAL(trim("trailing "), "trailing");
+    EXPECT_EQUAL(trim("   multiple leading and trailing  "), "multiple leading and trailing");
+    EXPECT_EQUAL(trim(" inner  space "), "inner  space");
+
+    /* Trim from beginning */
+    EXPECT_EQUAL(trimStart(""), "");
+    EXPECT_EQUAL(trimStart(" "), "");
+    EXPECT_EQUAL(trimStart("\ttab"), "tab");
+    EXPECT_EQUAL(trimStart("\nnewline"), "newline");
+    EXPECT_EQUAL(trimStart("\t\n"), "");
+
+    EXPECT_EQUAL(trimStart(" leading"), "leading");
+    EXPECT_EQUAL(trimStart("trailing "), "trailing ");
+    EXPECT_EQUAL(trimStart("   multiple leading and trailing  "), "multiple leading and trailing  ");
+    EXPECT_EQUAL(trimStart(" inner  space"), "inner  space");
+
+    /* Trim from end */
+    EXPECT_EQUAL(trimEnd(""), "");
+    EXPECT_EQUAL(trimEnd(" "), "");
+    EXPECT_EQUAL(trimEnd("tab\t"), "tab");
+    EXPECT_EQUAL(trimEnd("newline\n"), "newline");
+    EXPECT_EQUAL(trimEnd("\t\n"), "");
+
+    EXPECT_EQUAL(trimEnd(" leading"), " leading");
+    EXPECT_EQUAL(trimEnd("trailing "), "trailing");
+    EXPECT_EQUAL(trimEnd("   multiple leading and trailing  "), "   multiple leading and trailing");
+    EXPECT_EQUAL(trimEnd("inner  space "), "inner  space");
 }
